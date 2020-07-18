a2c588811bfb4220f2aef359633f6e101ba68c66
vinzce <rc_dom@protonmail.ch>
2020-07-18 10:10:12 +0200

    sqlite3 added

--- people.rb
+++ people.rb
@@ -1,12 +1,31 @@
 require 'faker'
+require 'sqlite3'
 
-x = 0
-names = []
+$db = SQLite3::Database.open 'people.db'
+$db.execute "CREATE TABLE IF NOT EXISTS people(firstname VARCHAR(255), lastname VARCHAR(255))"
 
-while x <= 10
-  names.push(Faker::Name.first_name)
-  x += 1
+
+def self.create_people(var)
+  x = 0
+  names = []
+
+  while x < var
+    names.push(Faker::Name.first_name)
+    x += 1
+  end
+
+  names.each do |name|
+    $db.execute "INSERT INTO people (firstname, lastname) VALUES(?, ?)", name, 'Tale'
+  end
+
+  puts "Task complete"
+
 end
 
-names.each {|name| puts "Hello, #{name.capitalize}"}
+def self.show_people
 
+  $db.execute "SELECT * FROM people" do |row|
+    p row
+  end
+
+end
